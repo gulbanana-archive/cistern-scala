0634e1b02d94de9b24e19c2eaa8067633fe60cbf
Thomas Castiglione <castiglione@gmail.com>
2013-02-06 01:01:48 +0800

    project stuff- replaced anorm with slick

--- project/Build.scala
+++ project/Build.scala
@@ -8,9 +8,8 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    // Add your project dependencies here,
     jdbc,
-    anorm
+   "com.typesafe.slick" %% "slick" % "1.0.0-RC2"
   )
 
 
