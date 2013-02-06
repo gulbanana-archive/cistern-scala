c80f97ae973e40939b645a21ef3e14370ae210b4
Thomas Castiglione <castiglione@gmail.com>
2013-02-07 00:55:52 +0800

    update to play 2.1 final

--- project/Build.scala
+++ project/Build.scala
@@ -9,12 +9,11 @@ object ApplicationBuild extends Build {
 
   val appDependencies = Seq(
     jdbc
-   //"com.typesafe.slick" %% "slick" % "1.0.0-RC2"
   )
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     // play-slick is a preview version of the slick integration coming in 2.2      
-  ).dependsOn(RootProject( uri("git://github.com/freekh/play-slick.git") ))
+  ).dependsOn(RootProject( uri("git://github.com/gulbanana/play-slick.git") ))
 
 }
