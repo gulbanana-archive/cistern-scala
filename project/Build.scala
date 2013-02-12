1d96b9a833dc412133f30b7af06c7b3b623ed744
Thomas Castiglione <castiglione@gmail.com>
2013-02-12 21:51:18 +0800

    psql prep

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,8 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    jdbc
+    jdbc,
+    "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
   )
 
 
